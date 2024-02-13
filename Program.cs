66c5b0a3c95d1900b37d545b28e04a455264394b
Max <Max>
2024-02-13 23:47:18 +0300

    finished 2nd task

--- Program.cs
+++ Program.cs
@@ -1,8 +1,34 @@
-﻿Console.Write("Введите число: ");
-int number = int.Parse(Console.ReadLine());
+﻿//1st Task
 
-if (number % 7 == 0 && number % 23 == 0){
-    Console.WriteLine($"{number} является числом, кратным 7 и 23");
-}else{
-    Console.WriteLine($"{number} НЕ является числом, кратным 7 и 23");
-}
+// Console.Write("Введите число: ");
+// int number = int.Parse(Console.ReadLine());
+
+// if (number % 7 == 0 && number % 23 == 0){
+//     Console.WriteLine($"{number} является числом, кратным 7 и 23");
+// }else{
+//     Console.WriteLine($"{number} НЕ является числом, кратным 7 и 23");
+// }
+
+// 2nd Task
+
+// Console.Write("Введите координаты точки Х: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите координаты точки Y: ");
+// int y = Convert.ToInt32(Console.ReadLine());
+
+// if(x == 0 || y == 0){
+//     System.Console.WriteLine("Для получения координатной четверти ни одна из точек не может находиться на оси, т.е. иметь значение 0");
+// }else{
+//     if(x > 0 && y > 0){
+//         System.Console.WriteLine("1");
+//     } else if(x > 0 && y < 0){
+//         System.Console.WriteLine("4");
+//     } else if(x < 0 && y < 0){
+//         System.Console.WriteLine("3");
+//     } else if(x < 0 && y > 0){
+//         System.Console.WriteLine("2");
+//     }
+// }
+
+//3rd Task
