e36a7b63eac7781ea53af663584a66abae7f9d4e
Max <Max>
2024-02-13 21:08:58 +0300

    Finished 1st task

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 7 == 0 && number % 23 == 0){
+    Console.WriteLine($"{number} является числом, кратным 7 и 23");
+}else{
+    Console.WriteLine($"{number} НЕ является числом, кратным 7 и 23");
+}
